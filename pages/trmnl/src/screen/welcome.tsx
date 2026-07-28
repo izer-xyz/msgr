1e45a6ea2b6b0ce3ee79a1a64e889f5bd8ee5fee
Péter <peter@perenyi.net>
2026-07-28 07:59:26 +0000

    Show date & time on welcome

--- pages/trmnl/src/screen/welcome.tsx
+++ pages/trmnl/src/screen/welcome.tsx
@@ -1,12 +1,22 @@
 import { ImageResponse } from "@cloudflare/pages-plugin-vercel-og/api";
 
 export default async function screen(device, env) {
-  let date = new Date().toLocaleTimeString('fr-FR', { timeZone: 'Indian/Reunion' }); 
+  let date = new Date().toLocaleDateString('fr-FR', {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+      timeZone: 'Indian/Reunion'
+  });
+  let time = new Date().toLocaleTimeString('fr-FR', {
+      timeStyle: 'short',
+      timeZone: 'Indian/Reunion'
+  });
   return new ImageResponse(
     <div tw="flex h-full w-full flex-col justify-center bg-black p-20">
       <div tw="flex flex-col">
-        <h1 tw="m-0 text-9xl font-bold leading-none tracking-tighter text-white"> { date } </h1>
-        <h1 tw="m-0 text-9xl font-bold leading-none tracking-tighter text-white">Welcome </h1>
+        <h1 tw="m-0 text-9xl font-bold leading-none text-white"> { time } </h1>
+        <h1 tw="m-0 text-9xl font-bold leading-none text-white captialize"> { date } </h1>
       </div>
     </div>
     ,  
