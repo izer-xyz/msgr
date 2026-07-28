2bc2b3f20b5c25386ed349d550308a0434e0ece1
Péter <peter@perenyi.net>
2026-07-28 01:42:18 +0000

    Disable error handling to show error trace

--- pages/trmnl/functions/api/display.ts
+++ pages/trmnl/functions/api/display.ts
@@ -6,33 +6,27 @@ export async function onRequest({ request, env }) {
 
   const device = await lookup(request.headers, env.TRMNL_DEVICES, true); 
   
-  try {
-
-    const filename = await process(device, env); 
-    
-    let response = JSON.stringify({
-      'filename': filename,
-      //'firmware_url': null,
-      //'firmware_version': null,
-      'image_url': `${ request.url }/../img/${ filename }`,
-      //'image_url_timeout': 0,
-      //'maximum_compatibility': false,
-      'refresh_rate': device.refresh_rate,
-      'reset_firmware': false,
-      //'special_function': 'none',
-      //'temperature_profile': 'default',
-      //'touchbar_mode': 'tap',
-      'update_firmware': false
-    });
-    console.log(`[${ device.id }] ${ response }`);  
-    return new Response(response, {
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-      }
-    });
-  } catch (err) {
-    console.log(`[${ device.id }] ${ err }`); 
-    return new Response('Error', { status: 400 });
-  }
+  const filename = await process(device, env); 
+  
+  let response = JSON.stringify({
+    'filename': filename,
+    //'firmware_url': null,
+    //'firmware_version': null,
+    'image_url': `${ request.url }/../img/${ filename }`,
+    //'image_url_timeout': 0,
+    //'maximum_compatibility': false,
+    'refresh_rate': device.refresh_rate,
+    'reset_firmware': false,
+    //'special_function': 'none',
+    //'temperature_profile': 'default',
+    //'touchbar_mode': 'tap',
+    'update_firmware': false
+  });
+  console.log(`[${ device.id }] ${ response }`);  
+  return new Response(response, {
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    }
+  });
 
 };
