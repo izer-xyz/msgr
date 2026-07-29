8c4f20c804cbf93aba8abecfc9328dff0fee5c87
Péter <peter@perenyi.net>
2026-07-29 02:55:03 +0000

    Roll back takumi - can't handle high resolution in cloudflare worker

--- pages/trmnl/src/screen/welcome.tsx
+++ pages/trmnl/src/screen/welcome.tsx
@@ -1,5 +1,4 @@
-//import { ImageResponse } from "@cloudflare/pages-plugin-vercel-og/api";
-import { ImageResponse } from "takumi-js/response"; 
+import { ImageResponse } from "@cloudflare/pages-plugin-vercel-og/api";
 
 export default async function screen(device, env) {
   let date = new Date().toLocaleDateString('fr-FR', {
