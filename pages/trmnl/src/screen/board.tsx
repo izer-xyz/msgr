98ea96fb7bd29a0a70d670f507df134b129f884f
Péter <peter@perenyi.net>
2026-07-30 12:26:32 +0000

    Fix board design

--- pages/trmnl/src/screen/board.tsx
+++ pages/trmnl/src/screen/board.tsx
@@ -2,10 +2,13 @@ import { render } from "takumi-js";
 
 export default async function screen(device, env) {
   let date = new Date().toLocaleDateString("fr-FR", {
-    weekday: "long",
     year: "numeric",
     month: "long",
     day: "numeric",
+    timeZone: "Indian/Reunion",
+  });
+  let day = new Date().toLocaleDateString("fr-FR", {
+    weekday: "long",
     timeZone: "Indian/Reunion",
   });
   let time = new Date().toLocaleTimeString("fr-FR", {
@@ -13,39 +16,50 @@ export default async function screen(device, env) {
     timeZone: "Indian/Reunion",
   });
   return render(
-    <div tw="flex h-full w-full flex-col bg-white p-20 text-6xl">
+    <div tw="flex h-full w-full flex-col bg-white text-black text-5xl px-5 leading-5">
       <div tw="flex">
-        <h1 tw="m-0 grow text-7xl font-bold leading-none text-black capitalize">
-          {date}
-        </h1>
-        <h1 tw="m-0 text-9xl font-bold leading-none text-black"> {time} </h1>
+        <h1 tw="flex grow">22:00</h1>
+        <h1 tw="capitalize">{date}</h1>
       </div>
-      <div tw="flex">
+      <div tw="flex pt-4">
         <div tw="flex-1 flex flex-col">
-          <div tw="font-bold self-center">Ajoudhui</div>
-          <div tw="flex pt-10">
-            <span tw="flex-1">09:45</span>
-            <span tw="flex-4">Ajoudhui fdfd fdfdf ddd dfdfd df d fdf f</span>
+          <h2 tw="flex capitalize font-bold m-0 p-0">
+            <span tw="w-15"></span>
+            {day}
+          </h2>
+          <div tw="flex pt-4">
+            <span tw="w-15 flex-none text-right px-2">-</span>
+            <span tw="">Anniversaire de Ming</span>
           </div>
-          <div tw="flex pt-10">
-            <span tw="flex-1">15:58</span>
-            <span tw="flex-4">Ajoudhui fdfd fdfdf ddd dfdfd df d fdf f</span>
+          <div tw="flex pt-4">
+            <span tw="w-15 flex-none text-right px-2">8:45</span>
+            <span tw="">Check-up avec nurse</span>
+          </div>
+          <div tw="flex pt-4">
+            <span tw="w-15 flex-none text-right px-2">10:30</span>
+            <span tw="">
+              Rdv avec la docteur B en ville. La bus part a 10:40.
+            </span>
+          </div>
+          <div tw="flex pt-4">
+            <span tw="w-15 flex-none text-right px-2">12:30</span>
+            <span tw="">Repas avec Jule a la maison.</span>
           </div>
         </div>
         <div tw="flex-1 flex flex-col">
-          <div tw="flex flex-col p-10">
-            <span>
-              Messages fdj fd kdfjkd fd fdf df djf dkjfkdjf dfkd fkdf dfjkdfkd
-              j-- fdf df fd d df df
+          <div tw="flex flex-col pl-4">
+            <span tw="border-black border-2 px-2 py-1 rounded-lg">
+              Bonjour Mama - Je te suite une tres belle journee d'ete avec tous
+              les mondes!
             </span>
-            <span tw="self-end">- Vivi</span>
+            <span tw="self-end">Viv' </span>
           </div>
-          <div tw="flex flex-col p-10">
-            <span>
-              Messages fdj fd kdfjkd fd fdf df djf dkjfkdjf dfkd fkdf dfjkdfkd
+          <div tw="flex flex-col pl-4 pt-4">
+            <span tw="border-black border-2 px-2 py-1 rounded-lg">
+              fdj fd kdfjkd fd fdf df djf dkjfkdjf dfkd fkdf dfjkdfkd
               jfkjdkfjdfjkd fk-- fdf df fd d df df
             </span>
-            <span tw="self-end">- Vivi</span>
+            <span tw="self-end">Gustave </span>
           </div>
         </div>
       </div>
@@ -53,7 +67,7 @@ export default async function screen(device, env) {
     {
       width: Number(device.width),
       height: Number(device.height),
-      format: 'raw'
+      format: "raw",
     },
   );
 }
