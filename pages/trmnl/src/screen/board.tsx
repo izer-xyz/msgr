f452e711e24a6e845a28dca5757d2cf45a04461c
Péter <peter@perenyi.net>
2026-07-31 11:49:29 +0000

    Make it bolder

--- pages/trmnl/src/screen/board.tsx
+++ pages/trmnl/src/screen/board.tsx
@@ -21,44 +21,45 @@ export default async function screen(device, env) {
     //timeZone: "Indian/Reunion",
   });
   return render(
-    <div tw="flex h-full w-full flex-col bg-white font-[Noto_Sans] text-black text-5xl leading-5">
-      <div tw="flex px-5">
-        <h3 tw="grow capitalize self-end">
+    <div tw="flex h-full w-full flex-col bg-white font-[Noto_Sans] font-black text-black text-6xl px-1 leading-5">
+      <div tw="flex text-2xl pb-8">
+        <div tw="grow capitalize self-end ">
           {day}, {date}
-        </h3>
-        <h1 tw="font-900 ">{time}</h1>
+        </div>
+        <div tw="">{time}</div>
       </div>
-      <div tw="flex pt-4 px-5 font-bold">
+      <div tw="flex">
         <div tw="flex-1 flex flex-col">
-          <div tw="flex ">
-            <span tw="pr-12"></span>
+          <div tw="bg-gray-200 p-1 rounded-lg">
+            <span tw=""></span>
             <span tw="">Anniversaire de Ming</span>
           </div>
-          <div tw="flex pt-4">
-            <span tw="pr-2 flex-none">08:11</span>
+          <div tw="mt-5 bg-gray-200 p-1 rounded-lg">
+            <span tw="text-gray-500">08:11 </span>
             <span tw="">Check-up avec nurse</span>
           </div>
-          <div tw="flex pt-4">
-            <span tw="pr-2 flex-none">10:30</span>
+          <div tw="mt-5 bg-gray-200 p-1 rounded-lg">
+            <span tw="text-gray-500">10:30 </span>
             <span tw="">
               Rdv avec la docteur B en ville. La bus part a 10:40.
             </span>
           </div>
-          <div tw="flex pt-4">
-            <span tw="pr-2 flex-none">12:30</span>
+          <div tw="mt-5 bg-gray-200 p-1 rounded-lg">
+            <span tw="text-gray-500">12:30 </span>
             <span tw="">Repas avec Jule a la maison.</span>
           </div>
         </div>
-        <div tw="flex-1 flex flex-col">
+        <div tw="flex-1 flex flex-col text-xs font-bold">
           <div tw="flex flex-col pl-4">
-            <span tw="border-black border-2 px-2 py-1 rounded-lg">
+            <span tw="bg-gray-200 p-2 rounded-lg">
               Bonjour Mama - Je te suite une tres belle journee d'ete avec tous
               les mondes!
             </span>
             <span tw="self-end">Viv' </span>
           </div>
+
           <div tw="flex flex-col pl-4 pt-4">
-            <span tw="border-black border-2 px-2 py-1 rounded-lg">
+            <span tw="bg-gray-200 p-2 rounded-lg">
               fdj fd kdfjkd fd fdf df djf dkjfkdjf dfkd fkdf dfjkdfkd
               jfkjdkfjdfjkd fk-- fdf df fd d df df
             </span>
@@ -71,7 +72,7 @@ export default async function screen(device, env) {
       width: Number(device.width),
       height: Number(device.height),
       format: "raw",
-      fonts: googleFonts(["Noto Sans"]),
+      fonts: googleFonts([{name: "Noto Sans", weight: "800..900"}]),
     },
   );
 }
