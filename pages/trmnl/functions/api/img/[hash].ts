baac1c1287e9ba798970f30f133beb3538d3057f
Péter <peter@perenyi.net>
2026-08-09 12:22:04 +0000

    Remove error handling.

--- pages/trmnl/functions/api/img/[hash].ts
+++ pages/trmnl/functions/api/img/[hash].ts
@@ -3,17 +3,7 @@ import { default as lookup, DEFAULTS } from "../../../src/device.ts";
 import process from "../../../src/image.ts";
 
 export async function onRequest({ request, env }) {
-  try {
-    const device = await lookup(request.headers, env.TRMNL_DEVICES);
-    const png = await process(device, env);
-    return new Response(png, { headers: { "Content-Type": "image/png" } });
-  } catch (err) {
-    console.log(
-      `[ERROR /api/img/${request.headers.get("id")}] ${err}`,
-    );
-    return new Response(
-      error(DEFAULTS, env, { message: "Image not found" }), 
-      { headers: { "Content-Type": "image/png" }, status: 404 }
-    );
-}
+  const device = await lookup(request.headers, env.TRMNL_DEVICES);
+  const png = await process(device, env);
+  return new Response(png, { headers: { "Content-Type": "image/png" } });
 }
