c128669f040474d60b21d1fe383237108962f5fa
Péter <peter@perenyi.net>
2026-08-11 12:21:31 +0000

    Add preview URL log

--- pages/admin/functions/api/admin/preview.js
+++ pages/admin/functions/api/admin/preview.js
@@ -2,6 +2,7 @@ export async function onRequestGet({ request }) {
 	const { search } = new URL(request.url);
 	let device = search.substring(1);
 
+	console.log(`[INFO /api/admin/preview] ${request.url}/../img/${device}.png`);
 	return await fetch(`${request.url}/../img/${device}.png`, {
 		headers: {
 			ID: device,
