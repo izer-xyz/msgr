d0a73e21481d89110b49ad231a59676a8d8fbc95
Péter <peter@perenyi.net>
2026-08-26 12:01:11 +0000

    Swap fast-png with @cf-wasm/png and clean-up code

--- workers/trmnl-img/src/index.js
+++ workers/trmnl-img/src/index.js
@@ -1,5 +1,6 @@
+import { encode, ColorType } from "@cf-wasm/png";
+
 import { from } from "../../../src/device.js";
-import { encode } from "fast-png";
 import welcome from "./welcome.js";
 import board from "./board.js";
 
@@ -19,36 +20,29 @@ export default {
     }
 
     let screen = SCREENS[device.screen] || SCREENS.welcome;
-    let png = encode(greyscale(device, await screen(device, path, env)));
+    let raw = greyscale(device, await screen(device, path, env));
+    let png = encode(raw, device.width, device.height, {
+      color: ColorType.Grayscale,
+      depth: Number(device.depth),
+    });
 
     return new Response(png, { headers: { "Content-Type": "image/png" } });
   },
 };
 
 function greyscale(device, data) {
-  let out = {
-    depth: Number(device.depth),
-    channels: 1,
-    width: Number(device.width),
-    height: Number(device.height),
-    data: null,
-  };
-  out.data = new Uint8Array(
-    new Array((out.width * out.height * out.depth) / 8),
-  );
+  let size = Number(device.width) * Number(device.height);
+  let depth = Number(device.depth);
 
-  let channels = data.length / (out.width * out.height);
+  let out = new Uint8Array((size * depth) / 8);
+  let channels = data.length / size;
 
-  for (let i = 0; i < out.width * out.height; i++) {
-    let grey =
-      0.2126 * data[i * channels] +
-      0.7152 * data[i * channels + 1] +
-      0.0722 * data[i * channels + 2];
-
-    out.data[Math.floor((i * out.depth) / 8)] |=
-      (grey >> (8 - out.depth)) <<
-      (8 - out.depth * (1 + (i % (8 / out.depth))));
+  for (let i = 0; i < size; i++) {
+    // let grey = 0.2126 * data[i * channels] + 0.7152 * data[i * channels + 1] + 0.0722 * data[i * channels + 2];
+    let index = i * channels;
+    let grey = (data[index] + data[index + 1] + data[index + 2]) / 3;
+    out[Math.floor((i * depth) / 8)] |=
+      (grey >> (8 - depth)) << (8 - depth * (1 + (i % (8 / depth))));
   }
-
   return out;
 }
