428bde422830cac3eb42758554d7a6aec0c05c48
Péter <peter@perenyi.net>
2026-09-08 22:12:05 +0000

    Fix column separator

--- img/src/board.js
+++ img/src/board.js
@@ -48,7 +48,7 @@ async function screen(device, params, env) {
         <div tw="w-[880px] flex flex-col font-bold">
           ${render_messages(messages)}
         </div>
-        <div tw="w-[880px] flex flex-col">
+        <div tw="w-[880px] flex flex-col border-l-[2px] border-gray-500 p-[16px] h-[80vh]">
           ${render_events(events)}
         </div>
       </div>
@@ -70,7 +70,7 @@ async function screen(device, params, env) {
 function render_events(events) {
   return events.reduce(
     (html, event) => `${html}
-            <div tw="mb-[44px] border-l-[16px] border-gray-500 p-[16px] h-[80vh]">
+            <div tw="mb-[44px]">
               <div tw="text-[60px] leading-[1em]"><span tw="text-gray-800">${event.time} </span>${event.subject}</div>
               <div tw="font-normal mt-[16px]" x-show="event.content">${event.content}</div>
             </div>`,
@@ -82,7 +82,7 @@ function render_messages(messages) {
   return messages.reduce(
     (html, message) => `${html}
             <div tw="mb-[44px] flex flex-col">
-              <span tw="border-b-[2px] border-gray-700 px-[24px] py-[16px]">${message.content}</span>
+              <span tw="border-b-[2px] border-gray-500 px-[24px] py-[16px]">${message.content}</span>
               <span tw="self-end text-[44px]">${message.from} </span>
             </div>`,
     "",
